20a755cb3ed4c9117c1622cc66353652133c46ca
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-12-13 11:04:25 +0900

    Update lecture4.py

--- lecture4.py
+++ lecture4.py
@@ -1,9 +1,2 @@
-import sys
-
-sys.setrecursionlimit(10**9)
-
-def summation(n):
-    if n == 1:
-        return 1
-    return n+summation(n-1)
-print(summation(20))
+L = [[each for each in range(9)] for eachNum in range(9)]
+print(L)
