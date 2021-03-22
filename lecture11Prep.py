54ed695a1d924027ce500e168b832f531fcabf7e
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2021-03-22 20:09:13 +0900

    Update lecture11Prep.py

--- lecture11Prep.py
+++ lecture11Prep.py
@@ -15,3 +15,13 @@ printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
+#Too inefficient to debug
+def printDataN(n):
+    printf("%dAnalysis datas available", n);
+printDataN(1000)
+printDataN(1000)
+printDataN(1000)
+printDataN(1000)
+printDataN(1000)
+printDataN(1000)
+printDataN(1000)
