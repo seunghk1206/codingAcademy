da9393da5054c617a64f85a97a709219f944cff9
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2021-03-23 20:13:51 +0900

    Update lecture11Prep.py

--- lecture11Prep.py
+++ lecture11Prep.py
@@ -1,3 +1,4 @@
+'''
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
@@ -15,6 +16,7 @@ printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
 printf("%d Analysis datas available", 3742);
+'''
 #Too inefficient to debug
 def printDataN(n):
     printf("%dAnalysis datas available", n);
@@ -25,3 +27,13 @@ printDataN(1000)
 printDataN(1000)
 printDataN(1000)
 printDataN(1000)
+
+class math:
+    pi = 3.141592
+    e = 2.718
+    def sqrt(self, n):
+        return n**(1/2)
+    def areaOfCircle(self, r):
+        return self.pi*r**2
+    def volumeOfSphere(self, r):
+        return 4/3*self.pi*r**3
