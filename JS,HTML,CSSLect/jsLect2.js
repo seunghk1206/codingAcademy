9313af45c268d7b03dbc2451b810f0aec5673a3b
Noble-1206 <seunghk1206@gmail.com>
2021-05-16 22:15:52 +0900

    Update jsLect2.js

--- JS,HTML,CSSLect/jsLect2.js
+++ JS,HTML,CSSLect/jsLect2.js
@@ -1,7 +1,18 @@
+var names = ['SKT', 'LG', 'Samsung']
+var A = [11]
+A = A.concat(names)
+alert(A)
+A.unshift('hi')
+alert(A)
+A.splice(2, 0, '111')
+alert(A)
+alert(A.slice(2, 4))
 
-var x = '1'
-if(x === 1){
-    alert('my love')
-}else if(x == 1){
-    alert('is ~~~~')
-}
+A.shift()
+alert(A)
+A.pop()
+alert(A)
+A.sort()
+alert(A)
+A.reverse()
+alert(A)
